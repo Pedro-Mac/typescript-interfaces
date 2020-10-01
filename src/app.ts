f0abdfa9fdfa47e940bd5818c9eff058243236ba
KozaX <55134615+KozaX@users.noreply.github.com>
2020-10-01 13:21:32 +0200

    add interface as function type, add optional params and props

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,38 @@
-interface Greetable {
-  name: string;
+// type AddFn = (a: number, b: number) => number;
+interface AddFn {
+  (a: number, b: number): number /*this is an Interface with Function type*/;
+}
+
+let add: AddFn = (n1, n2) => {
+  return n1 + n2;
+};
+
+interface Named {
+  readonly name?: string;
+  outputName?: string;
+}
+
+interface Greetable
+  extends Named /*, AnotherInterface - this is possbile with interfaces but NOT WITH CLASSES*/ {
+  //name: string;
 
   greet(phrase: string): void;
 }
 
 class Person implements Greetable {
-  name: string;
   age = 30;
 
-  constructor(name: string) {
-    this.name = name;
+  constructor(public name?: string, public outputName?: string) {
+    if (name) this.name = name;
   }
 
   greet(phrase: string) {
-    console.log(phrase + ' ' + this.name);
+    console.log(phrase + ' ' + this.name + ' ' + this.outputName);
   }
 }
 
 let user1: Greetable; // It can be type Greetable because the class Person implements Greetable, therefore user1 does not have to be of type Person
 
-user1 = new Person('Monica');
+user1 = new Person('Max', 'Rex');
 
 user1.greet('Hi there, I am');
